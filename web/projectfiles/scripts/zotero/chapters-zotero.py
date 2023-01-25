b0d9deae0d6bda02de6c64201b3aeae9f210da68
James Smithies <jsmithies@gmail.com>
2023-01-25 20:33:17 +0000

    Added linkwrap to Zotero bib urls

--- web/projectfiles/scripts/zotero/chapters-zotero.py
+++ web/projectfiles/scripts/zotero/chapters-zotero.py
@@ -5,7 +5,7 @@ from pyzotero import zotero
 zot = zotero.Zotero('24279', 'user', 'q4Wn37R6q4P2RZTIfYuxYsei')
 
 collectionID = ('XI3XZQ9G') #chapters collectionID
-zot.add_parameters(content='bib', style='turabian-fullnote-bibliography', itemType='-attachment')
+zot.add_parameters(content='bib', linkwrap='1', style='turabian-fullnote-bibliography', itemType='-attachment')
 items = zot.collection_items(collectionID)
 
 bibliography = ''
