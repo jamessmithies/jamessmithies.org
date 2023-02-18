0d963f6a33e9c20ec20a51fed19efa053f504c50
James Smithies <jsmithies@gmail.com>
2023-02-18 22:03:47 +0000

    Removed Django admin make static functionality

--- web/projectfiles/index/management/commands/staticcommand.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from django.core.management.base import BaseCommand, CommandError
-
-import subprocess
-
-class Command(BaseCommand):
-	def handle(self, *args, **options):
-		subprocess.call(["sh", "makestatic.sh"])
