434fa8a5e1e937dc419f3766c6c775c02a5ae45c
James Smithies <jsmithies@gmail.com>
2025-12-11 17:50:20 +1100

    Added software section to Zotero bibliography

---
+++ web/projectfiles/zotero/scripts/software-zotero.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#https://pyzotero.readthedocs.io/en/latest/index.html?highlight=style#search-request-parameters-for-read-api-calls
+
+from pyzotero import zotero
+zot = zotero.Zotero('24279', 'user', 'q4Wn37R6q4P2RZTIfYuxYsei')
+
+collectionID = ('UGLUWKRW') #software collectionID 
+zot.add_parameters(content='bib', linkwrap='1', style='turabian-fullnote-bibliography', itemType='-attachment')
+items = zot.collection_items(collectionID)
+
+bibliography = ''
+for item in items:
+    bibliography += item + '\n'
+
+with open('/data/web/projectfiles/templates/zotero/software-bibliography.html', 'w') as f:
+    f.write(str(bibliography))
+
+
+
+
